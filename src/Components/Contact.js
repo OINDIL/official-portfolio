d80553714aa7b7736786ac849b8ca98ccd4b44e4
oindil <oindilgolder303@gmail.com>
2024-01-12 22:17:41 +0530

    added the email js , email functionality

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,6 +11,7 @@ function Contact() {
     emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID , form.current, process.env.REACT_APP_PUBLIC_KEY)
       .then((result) => {
           console.log(result.text);
+          
       }, (error) => {
           console.log(error.text);
       });
@@ -40,6 +41,7 @@ function Contact() {
                             <input type="email" name="user_email" />
                             <label>Message</label>
                             <textarea name="message" />
+                            <div id="result-display"></div>
                             <input type="submit" value="Send" />
                         </form>
                     </div>
